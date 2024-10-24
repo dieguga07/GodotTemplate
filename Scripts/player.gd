aff41307958365dcdb06f3692519346301701b67
dieguga07 <dieguga04@gmail.com>
2024-10-24 19:32:52 +0200

    State-machine idle and run add

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,37 +10,32 @@ func _ready():
 	state_machine.active = true
 	
 func _process(delta):
-	update_animation_parameters()
+	pass
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		state_machine["parameters/conditions/is_jumping"] = false
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		state_machine["parameters/conditions/is_jumping"] = true
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
+	   # Rote el sprite según la dirección
+		$Player_Sprite.flip_h = direction < 0
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		state_machine["parameters/conditions/is_jumping"] = false
 	move_and_slide()
+	update_animation(direction)
 	
-func update_animation_parameters():
-	if (velocity.x == 0):
-		state_machine["parameters/conditions/is_idle"] = true
-		state_machine["parameters/conditions/is_walking"] = false
-	else : 
-		state_machine["parameters/conditions/is_idle"] = false
-		state_machine["parameters/conditions/is_walking"] = true	
-		
-		
-		
+func update_animation(move):
+	
+	state_machine.set("parameters/Move/blend_position",move)		
 		
