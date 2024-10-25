4cb960a5501f55ce3727433b44c499f4af51f9c0
dieguga07 <dieguga04@gmail.com>
2024-10-25 10:10:04 +0200

    State-machine rework idle and attack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,10 +4,11 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -250.0
 
-@onready var state_machine : AnimationTree = $Player_State_Machine
+@onready var state_machine = $Player_State_Machine["parameters/playback"]
 
 func _ready():
-	state_machine.active = true
+	pass
+	#state_machine.active = true
 	
 func _process(delta):
 	pass
@@ -22,6 +23,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
+	if Input.is_action_just_pressed("attack"):
+		print("swing")
+		state_machine.travel("Attack")
+		#state_machine["parameters/conditions/swing"] = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,9 +38,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
-	update_animation(direction)
 	
-func update_animation(move):
 	
-	state_machine.set("parameters/Move/blend_position",move)		
-		
