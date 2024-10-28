da4b5c94f13ec6925f48ddf059f96318b2d1c1ce
dieguga07 <dieguga04@gmail.com>
2024-10-28 01:22:09 +0100

    State-machine idle run attack and jump add

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,42 +1,33 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -250.0
 
 @onready var state_machine = $Player_State_Machine["parameters/playback"]
 
-func _ready():
-	pass
-	#state_machine.active = true
-	
-func _process(delta):
-	pass
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-
-	# Handle jump.
+	
+	# Manejar salto.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("attack"):
-		print("swing")
-		state_machine.travel("Attack")
-		#state_machine["parameters/conditions/swing"] = true
+		state_machine.travel("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Manejar ataque.
+	if Input.is_action_just_pressed("attack"):
+		state_machine.travel("attack")
+
+	# Obtener la dirección de entrada y manejar el movimiento.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
-	   # Rote el sprite según la dirección
+		# Rote el sprite según la dirección
 		$Player_Sprite.flip_h = direction < 0
 		velocity.x = direction * SPEED
+		state_machine.travel("walk")  # Cambiar a caminar
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
-	
-	
