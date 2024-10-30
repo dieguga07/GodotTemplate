19517acec7abcd7bd9e44c71db1ac53613bc5f8e
dieguga07 <dieguga04@gmail.com>
2024-10-30 14:14:23 +0100

    scene 2 add

--- Scripts/portal.gd
+++ Scripts/portal.gd
@@ -6,4 +6,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("teleport...")
-		get_tree().change_scene_to_file(scene)
+		call_deferred("change_scene")
+		
+func change_scene():
+	get_tree().change_scene_to_file(scene)
