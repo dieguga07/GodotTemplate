9b9270765d795e3d6ccc98f5ed4f438ff4319039
dieguga07 <dieguga04@gmail.com>
2024-10-30 16:13:26 +0100

    magic bullet fix

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -3,22 +3,27 @@ extends Area2D
 @export var damage = 20
 
 var direction:Vector2
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(0.8).timeout
+	animated_sprite_2d.play("explosion")
+	set_direction(Vector2.ZERO)
+	await get_tree().create_timer(0.5).timeout
 	queue_free()
-	
 	
 func set_direction(bulletDirection):
 	direction = bulletDirection.normalized()
 	
 	
 func _physics_process(delta):
-	global_position += direction * speed * delta
+	if animated_sprite_2d.animation != "explosion":
+		global_position += direction * speed * delta
 
 
 
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
+		print("damage")
 		body.get_damage(damage)
 		queue_free()
