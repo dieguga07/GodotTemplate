a1b37760131132866c0e07003f11fefcfe6e1273
Diego Gutierrez vazquez <dieguga04@gmail.com>
2024-10-31 11:15:25 +0100

    Layer and mask fix

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+@onready var timer: Timer = $Timer
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	timer.start()
+	
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
